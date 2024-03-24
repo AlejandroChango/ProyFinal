60f42cc808dd4364b1df24007c447bf8763ba881
AlejandorCH Dev <alejandroch2193dev@gmail.com>
2024-03-24 16:49:25 -0500

    Actualizar 1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from 'styled-components';
+const App =()=>{
+    return(
+    <Contenedor>
+        <Titulo> Lista de Contactos hola mundo
+
+        </Titulo>
+
+    </Contenedor>
+    );
+
+        
+}
+
+const Contenedor=styled.div`
+margin: 40px;
+width: 90%;
+max-width:400px;
+background: #fff;
+padding: 40px;
+border-radius: 5px;
+text-align: center;
+`;
+
+const Titulo = styled.h2`
+margin-bottom:10px;
+`;
+export default App;
